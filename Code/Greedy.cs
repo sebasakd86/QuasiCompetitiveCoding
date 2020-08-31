aae1990c4a802c9aaeb28d761eca4eeaa407bd2c
sebasakd86 <narbaysebas@gmail.com>
2020-08-31 17:38:06 -0300

    Solved ActivitySelection problem

--- Code/Greedy.cs
+++ Code/Greedy.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Competitive.Code
 {
     public class Greedy
     {
+        /*
+            Given a Knapsack of capacity G, and X items with weight W and values V,
+            fill the sack with the max value possible.
+            Items can be splitted in parts.
+        */
         public static double Knapsack(int g, int[,] items)
         {
             int itemCount = items.GetLength(0);
@@ -23,10 +30,11 @@ namespace Competitive.Code
             MaxHeap<double> mh = new MaxHeap<double>();
 
             for (int i = 0; i < itemCount; i++)
-            {   
-                double valuePerWeight = (double) items[i, 0] / items[i, 1];
-                while(items[i,1]-- > 0){
-                    mh.Insert(valuePerWeight);                    
+            {
+                double valuePerWeight = (double)items[i, 0] / items[i, 1];
+                while (items[i, 1]-- > 0)
+                {
+                    mh.Insert(valuePerWeight);
                 }
             }
             while (g > 0 && !mh.IsEmpty())
@@ -36,6 +44,54 @@ namespace Competitive.Code
                 g--;
             }
             return ret;
+        }
+
+        /*
+            Given N activities with start & end times, get the max number of activities
+            that can be performed by a person, knowing a person can only perform 1 activity at a time.
+        */
+        public static int ActivitySelection(List<Activity> activities)
+        {
+            if (activities.Count < 2)
+                return activities.Count;
+            // *Sort activities by end time, so I can get the one that end sooners (thus maximizing the ammount of activities to perform)
+            activities.Sort();
+            int ret = 0;
+            int currentTime = 0;
+            var currentActivity = activities.FirstOrDefault(a => a.Start >= currentTime);
+            // * As long as an activity that starts after the current time exists, 
+            // * get the first one (since they're sorted, it will always be the one that ends first)                
+            // * could iterate through the array instead of using FirstOrDefault
+            while(currentActivity != null)
+            {
+                ret++;
+                currentTime = currentActivity.End;
+                currentActivity = activities.FirstOrDefault(a => a.Start >= currentTime);
+            }
+            return ret;
+        }
+    }
+    public class Activity : IComparable<Activity>
+    {
+        public int Start { get; }
+        public int End { get; }
+        public Activity(int start, int end)
+        {
+            if (start >= end)
+                throw new ArgumentOutOfRangeException("Start is greater or equal than end");
+            this.End = end;
+            this.Start = start;
+        }
+
+        public int Compassare(object x, object y)
+        {
+            int endX = ((Activity)x).End;
+            int endY = ((Activity)y).End;
+            return endX.CompareTo(endY);
+        }
+        public int CompareTo(Activity other)
+        {
+            return End.CompareTo(other.End);
         }
     }
 }
--- Tests/Test_Greedy.cs
+++ Tests/Test_Greedy.cs
@@ -1,5 +1,8 @@
 using Xunit;
 using Competitive;
+using System.Collections.Generic;
+using Competitive.Code;
+using System;
 
 namespace Competitive.Tests
 {
@@ -8,10 +11,71 @@ namespace Competitive.Tests
         [Fact]
         public void Knapsack()
         {
-            Assert.Equal(3, Competitive.Code.Greedy.Knapsack(1, new int[,]{{6,2}}));
-            Assert.Equal(6, Competitive.Code.Greedy.Knapsack(2, new int[,]{{6,2}}));
-            Assert.Equal(3, Competitive.Code.Greedy.Knapsack(1, new int[,]{{10,5},{3,1}}));
-            Assert.Equal(43, Competitive.Code.Greedy.Knapsack(10, new int[,]{{10,2},{5,3},{15,5},{7,7},{6,1},{18,4},{3,1}}));
+            Assert.Equal(3, Greedy.Knapsack(1, new int[,] { { 6, 2 } }));
+            Assert.Equal(6, Greedy.Knapsack(2, new int[,] { { 6, 2 } }));
+            Assert.Equal(3, Greedy.Knapsack(1, new int[,] { { 10, 5 }, { 3, 1 } }));
+            Assert.Equal(43, Greedy.Knapsack(10, new int[,] { { 10, 2 }, { 5, 3 }, { 15, 5 }, { 7, 7 }, { 6, 1 }, { 18, 4 }, { 3, 1 } }));
+        }
+        [Theory]
+        [MemberData(nameof(ActivitySelectionTestData))]
+        public void ActivitySelection(int expected, List<Activity> activities)
+        {
+            Assert.Equal(expected, Greedy.ActivitySelection(activities));
+        }
+        public static TheoryData<int, List<Activity>> ActivitySelectionTestData
+        {
+            get
+            {
+                var d = new TheoryData<int, List<Activity>>();
+                d.Add(0, new List<Activity>() { });
+                d.Add(1, new List<Activity>() { new Activity(1, 2) });
+                d.Add(1, new List<Activity>() { new Activity(1, 2), new Activity(1, 3) });
+                d.Add(2, new List<Activity>() { new Activity(1, 2), new Activity(2, 3) });
+                d.Add(2, new List<Activity>() { new Activity(1, 2), new Activity(2, 3), new Activity(2, 4) });
+                d.Add(2, new List<Activity>() { new Activity(1, 5), new Activity(1, 2), new Activity(2, 3), new Activity(2, 4) });
+                d.Add(4, new List<Activity>() { new Activity(1, 2), new Activity(3, 4), new Activity(0, 6), new Activity(5, 7), new Activity(8, 9), new Activity(5, 9) });
+                return d;
+            }
+        }
+        [Fact]
+        public void Activity_StartGreaterValue_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var a = new Activity(5, 1);
+            });
+        }
+
+        [Fact]
+        public void Activity_StartEqualValue_Throws()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var a = new Activity(5, 5);
+            });
+        }
+        [Fact]
+        public void Activity_CorrectValue()
+        {
+            int start = 5;
+            int end = 10;
+            var a = new Activity(start, end);
+            Assert.Equal(start, a.Start);
+            Assert.Equal(end, a.End);
+        }
+
+        [Fact]
+        public void Activity_IsSorted()
+        {
+            var l = new List<Activity>()
+            {
+                new Activity(1,2),
+                new Activity(2,3),
+                new Activity(0,1)
+            };
+            l.Sort();
+            for (int i = 0; i < l.Count; i++)
+                Assert.Equal(i + 1, l[i].End);
         }
     }
 }
